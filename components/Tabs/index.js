055d86b2f81394614ba6bbfe8045d92a443d4582
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-07-20 12:31:58 -0400

    added tabs components to page

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,18 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topics = document.querySelector(".topics"),
+  getTopics = axios
+    .get("https://lambda-times-backend.herokuapp.com/topics")
+    .then(response => {
+      response.data.topics.map(topic => {
+        const newTopic = document.createElement("div");
+        newTopic.classList.add("tab");
+        newTopic.textContent = topic;
+        topics.appendChild(newTopic);
+      });
+    })
+    .catch(err => {
+      console.log(`Error: ${err}`);
+    });
